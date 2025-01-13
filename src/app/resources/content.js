2f259db9cb74886af5e0615717c5012efe015f1c
varinmehta <mehtavarin2004@gmail.com>
2025-01-13 20:53:06 +0530

    Initial project setup

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,256 +1,309 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
+    firstName: "Varin",
+    lastName: "Mehta",
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
-    role:      'Design Engineer',
-    avatar:    '/images/avatar.jpg',
-    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
-}
+    role: "Third year Computer Engineering student",
+    avatar: "/images/avatar.jpg",
+    location: "Asia/Calcutta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    languages: ["English", "Gujarati", "Hindi"], // optional: Leave the array empty if you don't want to display languages
+};
 
 const newsletter = {
-    display: true,
+    display: false,
     title: <>Subscribe to {person.firstName}'s Newsletter</>,
-    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
-}
+    description: (
+        <>
+            I occasionally write about design, technology, and share thoughts on
+            the intersection of creativity and engineering.
+        </>
+    ),
+};
 
 const social = [
     // Links are automatically displayed.
     // Import new icons in /once-ui/icons.ts
     {
-        name: 'GitHub',
-        icon: 'github',
-        link: 'https://github.com/once-ui-system/nextjs-starter',
+        name: "GitHub",
+        icon: "github",
+        link: "https://github.com/varinmehta",
     },
     {
-        name: 'LinkedIn',
-        icon: 'linkedin',
-        link: 'https://www.linkedin.com/company/once-ui/',
+        name: "LinkedIn",
+        icon: "linkedin",
+        link: "https://www.linkedin.com/in/varinmehta/",
     },
     {
-        name: 'X',
-        icon: 'x',
-        link: '',
+        name: "X",
+        icon: "x",
+        link: "https://www.x.com/vrnmht",
     },
     {
-        name: 'Email',
-        icon: 'email',
-        link: 'mailto:example@gmail.com',
+        name: "Email",
+        icon: "email",
+        link: "mailto:varin.mehta22@spit.ac.in",
     },
-]
+];
 
 const home = {
-    label: 'Home',
+    label: "Home",
     title: `${person.name}'s Portfolio`,
     description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Design engineer and builder</>,
-    subline: <>I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive<br/> user experiences. After hours, I build my own projects.</>
-}
+    headline: <>Web developer and Competitive programmer</>,
+    subline: (
+        <>
+            I'm Varin, a third-year Computer Engineering student at{" "}
+            <InlineCode>Sardar Patel Institute of Technology</InlineCode>.
+            Outside of academics, I focus on problem-solving and working on my
+            projects.
+        </>
+    ),
+};
 
 const about = {
-    label: 'About',
-    title: 'About me',
+    label: "About",
+    title: "About me",
     description: `Meet ${person.name}, ${person.role} from ${person.location}`,
     tableOfContent: {
         display: true,
-        subItems: false
+        subItems: false,
     },
     avatar: {
-        display: true
+        display: false,
     },
     calendar: {
         display: true,
-        link: 'https://cal.com'
+        link: "https://cal.com/varinmehta",
     },
     intro: {
         display: true,
-        title: 'Introduction',
-        description: <>Selene is a Jakarta-based design engineer with a passion for transforming complex challenges into simple, elegant design solutions. Her work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
+        title: "Introduction",
+        description: (
+            <>
+                Varin is a Mumbai based engineering student with deep interests
+                in the fields of technology, finance and current-affairs. In the
+                technology domain, he expertises in full stack web development
+                and problem solving. In the finance domain, he is interested in
+                stock analysis and long term investing.
+            </>
+        ),
     },
     work: {
-        display: true, // set to false to hide this section
-        title: 'Work Experience',
+        display: false, // set to false to hide this section
+        title: "Work Experience",
         experiences: [
             {
-                company: 'FLY',
-                timeframe: '2022 - Present',
-                role: 'Senior Design Engineer',
+                company: "FLY",
+                timeframe: "2022 - Present",
+                role: "Senior Design Engineer",
                 achievements: [
-                    <>Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
-                    <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
+                    <>
+                        Redesigned the UI/UX for the FLY platform, resulting in
+                        a 20% increase in user engagement and 30% faster load
+                        times.
+                    </>,
+                    <>
+                        Spearheaded the integration of AI tools into design
+                        workflows, enabling designers to iterate 50% faster.
+                    </>,
                 ],
-                images: [ // optional: leave the array empty if you don't want to display images
+                images: [
+                    // optional: leave the array empty if you don't want to display images
                     {
-                        src: '/images/projects/project-01/cover-01.jpg',
-                        alt: 'Once UI Project',
+                        src: "/images/projects/project-01/cover-01.jpg",
+                        alt: "Once UI Project",
                         width: 16,
-                        height: 9
-                    }
-                ]
+                        height: 9,
+                    },
+                ],
             },
             {
-                company: 'Creativ3',
-                timeframe: '2018 - 2022',
-                role: 'Lead Designer',
+                company: "Creativ3",
+                timeframe: "2018 - 2022",
+                role: "Lead Designer",
                 achievements: [
-                    <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
-                    <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
+                    <>
+                        Developed a design system that unified the brand across
+                        multiple platforms, improving design consistency by 40%.
+                    </>,
+                    <>
+                        Led a cross-functional team to launch a new product
+                        line, contributing to a 15% increase in overall company
+                        revenue.
+                    </>,
                 ],
-                images: [ ]
-            }
-        ]
+                images: [],
+            },
+        ],
     },
     studies: {
         display: true, // set to false to hide this section
-        title: 'Studies',
+        title: "Education",
         institutions: [
             {
-                name: 'University of Jakarta',
-                description: <>Studied software engineering.</>,
+                name: "Sardar Patel Institute of Technology - University of Mumbai",
+                cgpa: "8.54",
+                description: <>Computer Engineering.</>,
+                extra: (
+                    <>
+                        Part of the Forum for Aspirig Computer Engineers (FACE)
+                        subcomittee
+                    </>
+                ),
             },
             {
-                name: 'Build the Future',
-                description: <>Studied online marketing and personal branding.</>,
-            }
-        ]
+                name: "Swami Vivekanand International School - ICSE",
+                percent: "97.67%",
+                achievements: <>Class Prefect.Ranked 3rd in School</>,
+            },
+        ],
     },
     technical: {
         display: true, // set to false to hide this section
-        title: 'Technical skills',
+        title: "Technical skills",
         skills: [
             {
-                title: 'Figma',
-                description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+                title: "Figma",
+                description: (
+                    <>
+                        Able to prototype in Figma with Once UI with unnatural
+                        speed.
+                    </>
+                ),
                 // optional: leave the array empty if you don't want to display images
                 images: [
                     {
-                        src: '/images/projects/project-01/cover-02.jpg',
-                        alt: 'Project image',
+                        src: "/images/projects/project-01/cover-02.jpg",
+                        alt: "Project image",
                         width: 16,
-                        height: 9
+                        height: 9,
                     },
                     {
-                        src: '/images/projects/project-01/cover-03.jpg',
-                        alt: 'Project image',
+                        src: "/images/projects/project-01/cover-03.jpg",
+                        alt: "Project image",
                         width: 16,
-                        height: 9
+                        height: 9,
                     },
-                ]
+                ],
             },
             {
-                title: 'Next.js',
-                description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+                title: "Next.js",
+                description: (
+                    <>
+                        Building next gen apps with Next.js + Once UI +
+                        Supabase.
+                    </>
+                ),
                 // optional: leave the array empty if you don't want to display images
                 images: [
                     {
-                        src: '/images/projects/project-01/cover-04.jpg',
-                        alt: 'Project image',
+                        src: "/images/projects/project-01/cover-04.jpg",
+                        alt: "Project image",
                         width: 16,
-                        height: 9
+                        height: 9,
                     },
-                ]
-            }
-        ]
-    }
-}
+                ],
+            },
+        ],
+    },
+};
 
 const blog = {
-    label: 'Blog',
-    title: 'Writing about design and tech...',
-    description: `Read what ${person.name} has been up to recently`
+    label: "Blog",
+    title: "Writing about finance and spirituality...",
+    description: `Read what ${person.name} has been up to recently`,
     // Create new blog posts by adding a new .mdx file to app/blog/posts
     // All posts will be listed on the /blog route
-}
+};
 
 const work = {
-    label: 'Work',
-    title: 'My projects',
-    description: `Design and dev projects by ${person.name}`
+    label: "Work",
+    title: "My projects",
+    description: `Design and dev projects by ${person.name}`,
     // Create new project pages by adding a new .mdx file to app/blog/posts
     // All projects will be listed on the /home and /work routes
-}
+};
 
 const gallery = {
-    label: 'Gallery',
-    title: 'My photo gallery',
+    label: "Gallery",
+    title: "My photo gallery",
     description: `A photo collection by ${person.name}`,
     // Images from https://pexels.com
     images: [
-        { 
-            src: '/images/gallery/img-01.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
+        {
+            src: "/images/gallery/img-01.jpg",
+            alt: "image",
+            orientation: "vertical",
         },
-        { 
-            src: '/images/gallery/img-02.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-02.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-03.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
+        {
+            src: "/images/gallery/img-03.jpg",
+            alt: "image",
+            orientation: "vertical",
         },
-        { 
-            src: '/images/gallery/img-04.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-04.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-05.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-05.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-06.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
+        {
+            src: "/images/gallery/img-06.jpg",
+            alt: "image",
+            orientation: "vertical",
         },
-        { 
-            src: '/images/gallery/img-07.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-07.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-08.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
+        {
+            src: "/images/gallery/img-08.jpg",
+            alt: "image",
+            orientation: "vertical",
         },
-        { 
-            src: '/images/gallery/img-09.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-09.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-10.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-10.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-11.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
+        {
+            src: "/images/gallery/img-11.jpg",
+            alt: "image",
+            orientation: "vertical",
         },
-        { 
-            src: '/images/gallery/img-12.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-12.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-13.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-13.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-        { 
-            src: '/images/gallery/img-14.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
+        {
+            src: "/images/gallery/img-14.jpg",
+            alt: "image",
+            orientation: "horizontal",
         },
-    ]
-}
+    ],
+};
 
 export { person, social, newsletter, home, about, blog, work, gallery };
